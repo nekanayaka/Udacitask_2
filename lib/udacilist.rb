1e4e0f6843fbe4d5dde004c515afdcdedd89b686
Nimna <nimna@nimna.me>
2016-05-11 05:42:33 +0000

    Validates the type of item and delete index given by the user.

--- lib/udacilist.rb
+++ lib/udacilist.rb
@@ -7,12 +7,20 @@ class UdaciList
   end
   def add(type, description, options={})
     type = type.downcase
-    @items.push TodoItem.new(description, options) if type == "todo"
-    @items.push EventItem.new(description, options) if type == "event"
-    @items.push LinkItem.new(description, options) if type == "link"
+    if type == "todo" || type == "event" || type == "link"
+      @items.push TodoItem.new(description, options) if type == "todo"
+      @items.push EventItem.new(description, options) if type == "event"
+      @items.push LinkItem.new(description, options) if type == "link"
+    else
+      raise UdaciListErrors::InvalidItemType, "Invalid item"
+    end
   end
   def delete(index)
-    @items.delete_at(index - 1)
+    if (index - 1) < @items.length
+      @items.delete_at(index - 1)
+    else
+      raise UdaciListErrors::IndexExceedsListSize, "Index exceeds the list size!"
+    end
   end
   def all
     puts "-" * @title.length
