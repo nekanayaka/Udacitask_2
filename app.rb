8d414b14683a1762e2d517479958a0ace0c84413
Nimna <nimna@nimna.me>
2016-05-12 15:34:26 +0000

    Updated.

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require 'chronic'
-# require 'colorize'
+require 'colorize'
 # Find a third gem of your choice and add it to your project
 require 'date'
 require_relative "lib/listable"
@@ -46,4 +46,4 @@ new_list.all
 # ------------------------
 new_list.filter("event")
 
-new_list.all
+# new_list.all
