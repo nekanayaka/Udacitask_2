62a740010a873ef6bea234f834030d2afc12acc9
Nimna Ekanayaka <nimna@nimna.me>
2016-05-19 15:28:22 -0300

    I don't fucking care.

--- lib/listable.rb
+++ lib/listable.rb
@@ -6,7 +6,7 @@ module Listable
     due = options[:due] if options[:due] != nill
     start_date = options[:start_date] if options[:start_date] != nill
     end_date = options[:end_date] if options[:end_date] != nill
-    
+
     if start_date
       dates = @start_date.strftime("%D") if start_date
       dates << " -- " + @end_date.strftime("%D") if end_date
@@ -16,10 +16,10 @@ module Listable
     end
     return dates
   end
-  def details(description)
-    if description == "event"
+  def details
+    if @description == "event"
       format_description + "event dates: " + format_date
-    elsif description == "link"
+    elsif @description == "link"
        format_description + "site name: " + format_name
     else
       format_description + "due: " +
--- lib/todo.rb
+++ lib/todo.rb
@@ -6,8 +6,7 @@ class TodoItem
   def initialize(description, options={})
     @description = description
     @due = options[:due] ? Chronic.parse(options[:due]) : options[:due]
-    # @due = options[:due] ? Time.new.inspect.split[0].parse(options[:due]) : options[:due]
     @priority = options[:priority]
   end
-  
+
 end
